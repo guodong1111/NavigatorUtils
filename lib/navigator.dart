0e5139c16a1656b0b523b1956baf984092d96663
Alice Wu <alice.wu@highlight.mobi>
2021-08-18 11:22:39 +0800

    [Commit] 升級hl_core版本號

--- lib/navigator.dart
+++ lib/navigator.dart
@@ -1,2 +1,6 @@
 export 'route_delegate.dart';
 export 'route_page.dart';
+export 'back_dispatcher.dart';
+export 'page_state_mapping.dart';
+export 'navigator_utils.dart';
+export 'navigator_manager.dart';
